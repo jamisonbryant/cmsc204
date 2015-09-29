aff81007dc092405b78bd60484f15b5aa50f711f
Jamison Bryant <jbryant@capitolwebsites.com>
2015-09-29 15:14:11 -0400

    Re-implemented BasicLinkedList w/o LinkedList backbone

--- Assignment2/src/edu/montgomerycollege/cmsc204/jbryant/BasicLinkedList.java
+++ Assignment2/src/edu/montgomerycollege/cmsc204/jbryant/BasicLinkedList.java
@@ -1,10 +1,8 @@
 package edu.montgomerycollege.cmsc204.jbryant;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
-public class BasicLinkedList<T> extends LinkedList<T>
+public class BasicLinkedList<T>
 {
     private Node<T> firstNode;
     private Node<T> lastNode;
@@ -42,13 +40,18 @@ public class BasicLinkedList<T> extends LinkedList<T>
         Node<T> newNode = new Node<T>(data);
         
         // Add node to end of list
-        this.add(data);
+        if (listSize == 0) {
+            // Set first and last nodes
+            firstNode = newNode;
+            lastNode = newNode;
+        } else {
+            // Set new last node
+            lastNode.nextNode = newNode;
+            lastNode = newNode;
+        }
         
         // Increment list size variable
         listSize++;
-        
-        // Update last node variable
-        lastNode = newNode;
         
         // Return current node list
         return this;
@@ -66,13 +69,18 @@ public class BasicLinkedList<T> extends LinkedList<T>
         Node<T> newNode = new Node<T>(data);
         
         // Add node to front of list
-        this.add(0, data);
+        if (listSize == 0) {
+            // Set first and last nodes
+            firstNode = newNode;
+            lastNode = newNode;
+        } else {
+            // Set new first node
+            newNode.nextNode = firstNode;
+            firstNode = newNode;
+        }
         
         // Increment list size variable
         listSize++;
-        
-        // Update first node variable
-        firstNode = newNode;
         
         // Return current node list
         return this;
@@ -82,11 +90,8 @@ public class BasicLinkedList<T> extends LinkedList<T>
     {
         // Check if node list is not empty
         if (listSize > 0) {
-            // Get first node in list
-            T firstNode = get(0);
-            
             // Return node data
-            return firstNode;
+            return firstNode.nodeData;
         } else {
             // Return null
             return null;
@@ -97,100 +102,141 @@ public class BasicLinkedList<T> extends LinkedList<T>
     {
         // Check if node list is not empty
         if (listSize > 0) {
-            // Get first node in list
-            T firstNode = get(0);
-            
-            // Remove node from list
-            remove(0);
-            
+            // Deep copy first node
+            Node<T> currentNode = firstNode;
+
+            // Update new first node
+            firstNode = firstNode.nextNode;
+
             // Decrement list size variable
             listSize--;
             
             // Return node data
-            return firstNode;
+            return currentNode.nodeData;
         } else {
             // Return null
             return null;
-        }        
+        }
     }
     
     public T getLast()
     {
         // Check if node list is not empty
         if (listSize > 0) {
-            // Get last node in list
-            T lastNode = get(listSize - 1);
-            
             // Return node data
-            return lastNode;
+            return lastNode.nodeData;
         } else {
             // Return null
             return null;
-        }       
+        }
     }
     
     public T retrieveLastElement()
     {
         // Check if node list is not empty
         if (listSize > 0) {
-            // Get last node in list
-            T lastNode = get(listSize - 1);
-            
-            // Remove node from list
-            remove(listSize - 1);
-            
+            Node<T> currentNode = firstNode;
+            Node<T> previousNode = null;
+
+            while (currentNode != null) {
+                if (currentNode.equals(lastNode)) {
+                    lastNode = previousNode;
+                    break;
+                }
+
+                previousNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+
             // Decrement list size variable
             listSize--;
-            
+
             // Return node data
-            return lastNode;
+            return currentNode.nodeData;
         } else {
             // Return null
             return null;
-        }        
+        }
     }
     
     public ArrayList<T> toArrayList()
     {
-        // Initialize node array list
         ArrayList<T> newList = new ArrayList<T>();
-        
-        // Initialize list iterator
-        ListIterator iterator = iterator();              
+        ListIterator iterator = iterator();
+        Node currentNode = firstNode;
         
         // Import node list into array list        
-        while (iterator.hasNext()) {
-            newList.add((T) iterator.next());
+        while (currentNode != null) {
+            newList.add((T) currentNode.nodeData);
+            currentNode = currentNode.nextNode;
         }
         
-        // Return array list
         return newList;
     }
     
     public ListIterator iterator()
     {
-        // Create new list iterator
-        ListIterator newIterator = new ListIterator();
-        
-        // Return node list iterator
-        return newIterator;
+        // Return new list iterator
+        return new ListIterator();
+    }
+
+    public void remove(T element, Comparator comparator)
+    {
+        Node<T> currentNode = firstNode;
+        Node<T> previousNode = null;
+
+        while (currentNode != null) {
+            if (comparator.compare(currentNode.nodeData, element) == 0) {
+                if (currentNode.equals(firstNode)) {
+                    firstNode = firstNode.nextNode;
+                } else if (currentNode.equals(lastNode)) {
+                    lastNode = previousNode;
+                } else {
+                    previousNode.nextNode = currentNode.nextNode;
+                }
+
+                return;
+            }
+
+            previousNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
     }
     
     public class ListIterator implements Iterator
     {
-        @Override
-        public boolean hasNext()
+        private Node<T> currentNode;
+
+        public ListIterator()
         {
-            // TODO: Auto-generated method stub
-            return false;
+            currentNode = firstNode;
         }
 
         @Override
-        public Object next()
+        public boolean hasNext()
         {
-            // TODO: Auto-generated method stub
-            return null;
-        }      
+            return currentNode != null;
+        }
+
+        @Override
+        public T next()
+        {
+            if (hasNext()) {
+                // Advance node pointer
+                Node<T> requestedNode = currentNode;
+                currentNode = currentNode.nextNode;
+
+                // Return requested node data
+                return requestedNode.nodeData;
+            } else {
+                throw new NoSuchElementException("next() called on iterator without subsequent member");
+            }
+        }
+
+        public void remove()
+        {
+            throw new UnsupportedOperationException("remove() not implemented for ListIterator inner class");
+        }
     }
     
     public class Node<T>
@@ -200,17 +246,12 @@ public class BasicLinkedList<T> extends LinkedList<T>
         
         public Node()
         {
-            // Initialize node data
             nodeData = null;
-            
-            // Initialize next node
             nextNode = null;
         }
-        
-        // TODO: Specify next node as parameter
+
         public Node(T data)
         {
-            // Initialize node data
             nodeData = data;
         }
     }
--- Assignment2/src/edu/montgomerycollege/cmsc204/jbryant/SortedLinkedList.java
+++ Assignment2/src/edu/montgomerycollege/cmsc204/jbryant/SortedLinkedList.java
@@ -1,31 +1,31 @@
-package edu.montgomerycollege.cmsc204.jbryant;
-
-/**
- * Sorted Linked List
- * 
- * Description goes here.
- * 
- * @author Jamison Bryant (jbryan46@montgomerycollege.edu) for CMSC 204 (R. Alexander) M/W 1PM-3PM
- */
-public class SortedLinkedList<T> extends BasicLinkedList<T>
-{
-    /**
-     * Adds an element to the end of the basic linked list
-     */    
-    public BasicLinkedList<T> addToEnd(T data)
-    {
-        throw new UnsupportedOperationException("addToEnd() is an invalid operation for this list");
-        
-    }
-    
-    /**
-     * Adds an element to the beginning of the basic linked list
-     * 
-     * @param data Element to add
-     * @return Reference to the list
-     */
-    public BasicLinkedList<T> addToFront(T data)
-    {
-        throw new UnsupportedOperationException("addToEnd() is an invalid operation for this list");
-    }
-}
+//package edu.montgomerycollege.cmsc204.jbryant;
+//
+///**
+// * Sorted Linked List
+// *
+// * Description goes here.
+// *
+// * @author Jamison Bryant (jbryan46@montgomerycollege.edu) for CMSC 204 (R. Alexander) M/W 1PM-3PM
+// */
+//public class SortedLinkedList<T> extends BasicLinkedList<T>
+//{
+//    /**
+//     * Adds an element to the end of the basic linked list
+//     */
+//    public BasicLinkedList<T> addToEnd(T data)
+//    {
+//        throw new UnsupportedOperationException("addToEnd() is an invalid operation for this list");
+//
+//    }
+//
+//    /**
+//     * Adds an element to the beginning of the basic linked list
+//     *
+//     * @param data Element to add
+//     * @return Reference to the list
+//     */
+//    public BasicLinkedList<T> addToFront(T data)
+//    {
+//        throw new UnsupportedOperationException("addToEnd() is an invalid operation for this list");
+//    }
+//}
